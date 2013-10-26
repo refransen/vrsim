0e7f4b02ff79692dac95b51eea3f33ee14459a6d
eltoky <frank@indie-zone.com>
2013-10-26 14:03:42 +0800

    Addition of linux tool

--- moodledata/licext/config_linux.php
+++ moodledata/licext/config_linux.php
@@ -1,12 +1,12 @@
 <?php //0046a
 if(!extension_loaded('ionCube Loader')){$__oc=strtolower(substr(php_uname(),0,3));$__ln='ioncube_loader_'.$__oc.'_'.substr(phpversion(),0,3).(($__oc=='win')?'.dll':'.so');if(function_exists('dl')){@dl($__ln);}if(function_exists('_il_exec')){return _il_exec();}$__ln='/ioncube/'.$__ln;$__oid=$__id=realpath(ini_get('extension_dir'));$__here=dirname(__FILE__);if(strlen($__id)>1&&$__id[1]==':'){$__id=str_replace('\\','/',substr($__id,2));$__here=str_replace('\\','/',substr($__here,2));}$__rd=str_repeat('/..',substr_count($__id,'/')).$__here.'/';$__i=strlen($__rd);while($__i--){if($__rd[$__i]=='/'){$__lp=substr($__rd,0,$__i).$__ln;if(file_exists($__oid.$__lp)){$__ln=$__lp;break;}}}if(function_exists('dl')){@dl($__ln);}}else{die('The file '.__FILE__." is corrupted.\n");}if(function_exists('_il_exec')){return _il_exec();}echo('Site error: the file <b>'.__FILE__.'</b> requires the ionCube PHP Loader '.basename($__ln).' to be installed by the website operator. If you are the website operator please use the <a href="http://www.ioncube.com/lw/">ionCube Loader Wizard</a> to assist with installation.');exit(199);
 ?>
-HR+cPqZiH88IIBsrxrRHeKlonkTEmzgLYwmBo+oCEbCIeDuTJdLh/QseNSO+1vF1nBIF5TRnrB0u
-T516apNlEo+qjfq7H+W6MvAsmS89i/lLKXbDY9vasJL4Pxpcn7pY/VpkQ6wgUFAm1XG67RHzEqLo
-jjdx3Uczu6h+57z+jVYIQeW8TVHLGHVjqP4Yelo/IhpFGdGIqECXqMdNJPdMYL0r67x7lw00miQY
-Buk+eDrJsaAST9pCfHchZAn+jOarlK5jdFyG94HvRAMpYaJOvA5Wur3sUAa20FFb3tJZ2qqiUFi+
-2/PxieNNhs7uawkIP8VGvccmh3B2gKHAUBiFE9NzI322imHf2XMMsQsMDMIibafMsY0urrfjkI1E
-R8MX5umE5EVq55jqPOES5GrwvjlhYcu34HItKR3Z+u4fBLujbg8vI/DjnbP0tCw/iGh+pb9XWYZv
-P73D1rd33BcORkzG+i/6XhCvJ1DFRnhzRgxEe7TvW/Zry/9ZjjIb/lFBvUPSlpJHRjGMqvcBcEKg
-QbE4L7gfEbKVidazlCfqk4YsfrXuWODmcAoPDh2mzFUwX+0D+PGaXrQVrFHOzN80NiqHAiTlvS3w
-UazMDfaeSb70+Vt+nmZ8TuzdqCv4S5PkKL5IbhcggOEsfG==
+HR+cPmlVEnNMEO1QgsMkDWOnhmcw0DsaHAXviRwyEsUbTsFe3fuRUSd8bFo3fvZaDi0BwU5RwN4+
+Jbyr1n7xEDJokQVVJyFmzjdlMnM8sS6kMBKcscdKhmkqzn2NFyuV3UtFBH4PHJH7GsMewLFWo7JU
+gmq5wFAakMKWlFemZ5yNrCnKASPK17JHikbqOa9Nqc3glONvXz9reZvDtlj27A336gNNve/dhJxX
+1nOwPsmtIPFCv6BY/BuIDpEL17tK+DwubK+x9JVkRREAHDZaeM3ZKFPugG80y+NsSB9perK/ziE7
+n7F2L+6lQYit5DDoaH/wdWH6gt0eh9kCWYxx3OVJD0Bt8/bqyXssSzZpLVKQidQRTTnvYavB6aRW
+AA0dIqYzpltACzcPTObP+l2xMlPVAoiWYmbn204NSf47NGjhaULVfLZqGQpDJAxjxuvISZsDmKrg
+8w0blOhXyr0Kh+4EVtBA2kD5KbVDnq96LBSVulsCR5SrQh86vHViXW8XrP2HYRAqr4OixwKkqnPf
+bQ89u0n23/E8JcaB47cFcyhtoeLiBk+pSuXRwlOhN88WDcqIg76PJO2nINpObwh4gJZ+QxrbKLBT
+DZ1msNBC7VaVTRRVBkNRO9OlDj3r4BPnq5fxgMMO1ZiC4gnLVTkr
--- moodledata/licext/config_win.php
+++ moodledata/licext/config_win.php
@@ -1,12 +1,12 @@
 <?php //0046a
 if(!extension_loaded('ionCube Loader')){$__oc=strtolower(substr(php_uname(),0,3));$__ln='ioncube_loader_'.$__oc.'_'.substr(phpversion(),0,3).(($__oc=='win')?'.dll':'.so');if(function_exists('dl')){@dl($__ln);}if(function_exists('_il_exec')){return _il_exec();}$__ln='/ioncube/'.$__ln;$__oid=$__id=realpath(ini_get('extension_dir'));$__here=dirname(__FILE__);if(strlen($__id)>1&&$__id[1]==':'){$__id=str_replace('\\','/',substr($__id,2));$__here=str_replace('\\','/',substr($__here,2));}$__rd=str_repeat('/..',substr_count($__id,'/')).$__here.'/';$__i=strlen($__rd);while($__i--){if($__rd[$__i]=='/'){$__lp=substr($__rd,0,$__i).$__ln;if(file_exists($__oid.$__lp)){$__ln=$__lp;break;}}}if(function_exists('dl')){@dl($__ln);}}else{die('The file '.__FILE__." is corrupted.\n");}if(function_exists('_il_exec')){return _il_exec();}echo('Site error: the file <b>'.__FILE__.'</b> requires the ionCube PHP Loader '.basename($__ln).' to be installed by the website operator. If you are the website operator please use the <a href="http://www.ioncube.com/lw/">ionCube Loader Wizard</a> to assist with installation.');exit(199);
 ?>
-HR+cP+e2YgsawWw2jRWVZl9438vHGgjr4Q8l/kWhKazVCGysqhN8O67gjMmQ6y7s0dmWQfInP6NP
-vezuUhrAfzX4s01uyEC80RYGcTg7UYauZ440MKKly/GfLnNUW70Xr5LS/u4PkCA1hAfxCCIJmMOe
-Z8Ny5jSXZo6DYjAfQxW15ujqaNXnznNIm4HBZ1+YgrsrRcm4BxhiRM54571joHBjl3csBXXHan3H
-tf0WRQoNCC9+cuF650phd/EO7tNu7RpLL6W2tIxhxwdtbj273hJRS8rC89zmPNb4SMbc5m409n7v
-7ivhmltmPs8rVXBdIT81foCv4qxO7RXcmJfFUa0ztlNdTEcyFkKwZj89rtInqFNbzwUMlU1Z/5tx
-FRKt0mo2TrgUlOC6WQpn5J/nqKJXAm33wkQMraYzLDY4VBBtivvVas1yXzRbcM0p6MCPwwW8CHKv
-uz3nqh2YO9rpgcyFlgLnItQ3W4+aryv6DO4XVQRp5OLqUHzhQDUzB4DBNhFkA0aQOtUJsun6jj0g
-we/thiOti6WTjTSwCrA/2CWX66h2OQ1U3xqYmQ2yPcaeG5xlFmFFLG1cWM+B8NXWuybS+FwQ9d4b
-qpdpuGPaQ8HVXdc+EfHafzIwwq3j2E9lm07a01P1L+2owDz7hQqgWnmM
+HR+cPuZies9HI1XLA5OsGP9jQwqKNfBqXNuxNl9rk9hf2MY9IgYG2EsHpZcci01Tu+2gVdhwrYqB
+xHPrGsCN3NQee4glcBqNdI6QClQRjJ5jDmd2bFx6sXbQta8oS+AKWcsM2oN5BVem8N6OT3sv4I+7
+X6owLfWAO0qCohQs072NWA+Q+EJGx4qr0Q+Q0MCwryozaaP6CrE6rtpvBMTIX8rb7JBgS9sRjdwe
+pep4lzR1CoDQXb+hdOKJ03X61h4lDdxSn+nExnmBBqopYaJOvA5Wur3sUAa20FFblsTzF+xvvlUV
+aEh9maVZhqZzoLLxVYKT/ovisLA5vmx6kC7/lIq+/XJ0KrA3enx1/0xzj62ib1p24foY4dGE82r4
+Wv2gvFNUfXADNtfHwBVCgbRij69A1da5qRKoZa9BBASf0gglOnobmY69Cui0+hQhpQh6QotCXXaD
+q+iYVG+k2EAfEdbsGOZA7u7xFKtQo3UeuGLjy692edMlh/pnpMXdJYyP14UI/uV9mYDImtLMwYeG
+JXVU820ktne7xKNAaB1KZRnPPye7pMmjyoA+fOkVPMYLl6oBqfJHV2gVHOpgXEhyODJICp2PG7el
+7FZxhZOczNQu4LAtxen9OxQ0HCg+8JcBYk7f24JF2GC6bQQ6Vec3
