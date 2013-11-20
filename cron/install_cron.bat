0a4111886572ed35013a2f7cfebd4b1d282242de
eltoky <frank@indie-zone.com>
2013-11-21 07:43:16 +0800

    Update cron
    Update cron to remove popup windows

--- cron/install_cron.bat
+++ cron/install_cron.bat
@@ -1,6 +1,6 @@
 @echo off
-schtasks /create /sc minute /mo 5 /tn "LMS User syncs" /tr C:\SBCServ\htdocs\moodle\cronb.bat
-schtasks /create /sc minute /mo 30 /tn "LMS Cron" /tr C:\SBCServ\htdocs\moodle\crona.bat
+schtasks /create /sc minute /mo 5 /tn "LMS User syncs" /tr C:\SBCServ\htdocs\moodle\cronb.bat /ru "SYSTEM" /f
+schtasks /create /sc minute /mo 30 /tn "LMS Cron" /tr C:\SBCServ\htdocs\moodle\crona.bat /ru "SYSTEM" /f
 
 C:\SBCServ\htdocs\moodle\cronb.bat
 C:\SBCServ\htdocs\moodle\crona.bat
