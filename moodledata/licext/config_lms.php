8c420fa0a71e7ffaf763ff274f683b540f90a1f9
eltoky <frank@indie-zone.com>
2013-11-22 05:42:53 -0800

    Addition of filedir adding file

--- moodledata/licext/config_lms.php
+++ moodledata/licext/config_lms.php
@@ -1,15 +1,6 @@
-<?php //0046a
-if(!extension_loaded('ionCube Loader')){$__oc=strtolower(substr(php_uname(),0,3));$__ln='ioncube_loader_'.$__oc.'_'.substr(phpversion(),0,3).(($__oc=='win')?'.dll':'.so');if(function_exists('dl')){@dl($__ln);}if(function_exists('_il_exec')){return _il_exec();}$__ln='/ioncube/'.$__ln;$__oid=$__id=realpath(ini_get('extension_dir'));$__here=dirname(__FILE__);if(strlen($__id)>1&&$__id[1]==':'){$__id=str_replace('\\','/',substr($__id,2));$__here=str_replace('\\','/',substr($__here,2));}$__rd=str_repeat('/..',substr_count($__id,'/')).$__here.'/';$__i=strlen($__rd);while($__i--){if($__rd[$__i]=='/'){$__lp=substr($__rd,0,$__i).$__ln;if(file_exists($__oid.$__lp)){$__ln=$__lp;break;}}}if(function_exists('dl')){@dl($__ln);}}else{die('The file '.__FILE__." is corrupted.\n");}if(function_exists('_il_exec')){return _il_exec();}echo('Site error: the file <b>'.__FILE__.'</b> requires the ionCube PHP Loader '.basename($__ln).' to be installed by the website operator. If you are the website operator please use the <a href="http://www.ioncube.com/lw/">ionCube Loader Wizard</a> to assist with installation.');exit(199);
-?>
-HR+cPq0aUjRCyruQZP/lzK+GQs0ovHk7nVaqKBMyGGeDX+C1X1+MW/ECZYOM3SIIu/daYBXdSOqG
-C7u4GLD9B00bEzNi5dA+ovl15p36Yq0eEPe4t08erpfBGSbn6nZPIqMLSV5cGTpOFNiPS8qSlNLR
-wfx59IjOtfHd3CXQBOX2G+2KqgnO1+c18KDAC453kRBq1ABQ5796eiwZ8TJqDmaOHXK0ZALN0Ywz
-BMzm2Rx8RaebriWEQWIehBZMN8XOnlQAGl7gq4M5eDGSCjOMrSOQW+01JlHNOgvZOp22mHwqZuRc
-+TovWg5yRaCSr5v/X/okANqBhU+LnLZuoicbDXEahzR/oQOinuQU1SLy52ho2x/xULLW6vX7JKTH
-iqvfpDoU2ahkH+/s8crMSFAhZh15k/NMW7sN0v0zwxZ7iPi//RqCs5/on+mmdtBo6+cUeaaKeGni
-acK0fO/iS0rWvZXzBQ9I7rP9ytboa6oVD5StGs9R63XwSlgBH4N2CiLge9vNlsN0q3qu9nTZwL0W
-l1FXL5a1cp1sUIJQnSCSFcXQX7EFI5S6QMyx4n1zGWeuFW6yoR1gD19M1cj5iwuEJm+zFVxsChp9
-1EOhllkmvN09mEOAWj9tHM1/GPolCMQlMxTEeALbEcOOdao0uqWxJIsz7RNsT9qbVxycIxSQkjmT
-YfnRzcVJGzLzjU6KWX2Tjgd1Bbjf7FGnYgV1Wf70GCo3M9Y1tF585soyZUvydCg30OKPKj0ds3yA
-Fp7scvmAEp8psHTUkwx3g2QFzjeM61He3x02oAo3EoSJ03x0ZJb6sXaItcfaI6RBPJz+Eq448aS3
-FjEiPIwh3c1tNm6zlkczBxC=
+<?php
+global $licAPIConfig;
+$licAPIConfig = array();
+$licAPIConfig["tools"] = "/home/lmsvrsim/public_html/vrsim.git/trunk/moodledata/licext/tools/getid";
+$licAPIConfig["md5"] = "da6a7375e037f38e1ca9596c73c46421";
+$licAPIConfig["licfolder"] = "/home/lmsvrsim/public_html/licenses/";
